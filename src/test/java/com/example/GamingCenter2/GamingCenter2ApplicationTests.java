7e64d5a83889a8fb605813de49438c32182098d6
muhamedcakaj <muhamedcakaj57@gmail.com>
2025-11-30 13:54:27 +0100

    NEW Project

---
+++ src/test/java/com/example/GamingCenter2/GamingCenter2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GamingCenter2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingCenter2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
